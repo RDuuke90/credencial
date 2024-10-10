0d7f4fa504e65e71b11f6c36a8fd573cc1d9c603
RDuuke90 <115588732+RDuuke90@users.noreply.github.com>
2024-10-09 19:00:29 -0500

    Update gestor.py

--- gestor.py
+++ gestor.py
@@ -1,4 +1,26 @@
-class Gestor:
+from credencial import Credencial
 
-    def agregar_credencial(self):
-        print("Metodo agregar!")
+class Gestor:
+    __credenciales: list = []
+
+    def listar_credenciales(self) -> None:
+        for credencial in self.__credenciales:
+            print("Credencial - servicio: ", credencial.servicio)
+    
+    def crear_credencial(self) -> Credencial:
+        print("Creando credencial")
+        servicio: str = input("Agregar el servicio: ")
+        usuario: str = input("Agregar el usuario: ")
+        contrasena: str = input("Agregar la contraseña: ")
+
+        credencial: Credencial = Credencial(
+            in_servicio=servicio,
+            in_usuario=usuario,
+            in_contrasena=contrasena
+        )
+        print("Credencial creada!")
+        return credencial
+    
+    def agregar_credencial(self, credencial: Credencial) -> None:
+        self.__credenciales.append(credencial)
+        print("Credencial agregada!")
